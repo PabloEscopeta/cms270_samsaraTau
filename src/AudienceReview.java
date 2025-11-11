18aeafe78381304cbb151ca91278d4650017b1a9
PabloEscopeta <144813749+PabloEscopeta@users.noreply.github.com>
2025-11-11 09:24:44 -0500

    AudienceReview Class elaboration
    AudienceReview class is defined with necessary base attributes (all of which are private access). A constructor method is created to allow the initialization /or creation of a new review.  NOTE: Depending on whether or not it's necessary we could add a method later to display all of the review information, though I am unsure if it would mesh well with the UI. Getters were also made for every attribute.
    
    (Don't mix coffee with energy drinks; My kidneys are paying the price for my hubris)

--- src/AudienceReview.java
+++ src/AudienceReview.java
@@ -1,6 +1,61 @@
 
 public class AudienceReview {
 	
-	// get audience reviews
+	private String gameName;
+	private String gameGenre;
+	private String gameReviewer;
+	private String reviewerID; 
+	private int userGameReviewScore;
+	private String userReviewComment;
+	private String userReviewDate; 
+	
+	// constructor
+	public AudienceReview(String newGameName, String newGameGenre, String newGameReviewer, 
+			String newReviewerID, int newUserGameReviewScore, String newUserReviewComment,
+			String newUserReviewDate) {
+		this.gameName = newGameName;
+		this.gameGenre = newGameGenre;
+		this.gameReviewer = newGameReviewer;
+		this.reviewerID = newReviewerID;
+		this.userGameReviewScore = newUserGameReviewScore;
+		this.userReviewComment = newUserReviewComment;
+		this.userReviewDate = newUserReviewDate;
+	}
+	
+	// getGameName
+	public String getGameName() {
+		return gameName;
+	}
+	
+	// getGameGenre
+	public String getGameGenre() {
+		return gameGenre;
+	}
+	
+	// getGameReviewer
+	public String getGameReviewer() {
+		return gameReviewer;
+	}
+	
+	// getReviewerID
+	public String getReviewerID() {
+		return reviewerID; 
+	}
+	
+	// get userGameReviewScore
+	public int getUserGameReviewScore() { 
+		return userGameReviewScore;
+	}
+	
+	// getUserReviewComment
+	public String getUserReviewComment() {
+		return userReviewComment;
+	}
+	
+	// getUserReviewDate
+	public String getUserReviewDate() {
+		return userReviewDate;
+	}
+	
 
 }
