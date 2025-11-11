3f4c8d28634c8b9dfdc243f8cdc2d6b1e632518d
PabloEscopeta <144813749+PabloEscopeta@users.noreply.github.com>
2025-11-11 09:42:42 -0500

    ReviewForum setup
    ReviewForum now handles the creation/initialization of an ArrayList & related methods.
    ArrayList will contain every Review in the forum / program.
    Method addReview() adds a new review to ArrayList when called; displayReview() prints out review info.
    
    NOTE: Since FX is yet to be implemented (as of this moment) displayReview() defaults to printing in the console. Once JavaFX does get set up on the front end, we need to (maybe) rework displayReview() so it functions specifically with the UI.

--- src/RollinsGameReviewForum.java
+++ src/RollinsGameReviewForum.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList; 
 
 public class RollinsGameReviewForum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Create ArrayList for reviews.
+	private ArrayList<AudienceReview> reviews = new ArrayList<>();
+	
+	// addReview mthod
+	public void addReview(AudienceReview review) {
+		reviews.add(review);
+	}
+	
+	// display review method
+	// prints out review in console as of this moment;
+		// rework when implementing UI / JavaFX
+	public void displayReview() {
+		for (AudienceReview review : reviews) {
+			System.out.println(review);
+		}
 	}
 
 }
