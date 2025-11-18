9dedeb241422429c6e1ed804690d8c1b6c9d7d75
PabloEscopeta <144813749+PabloEscopeta@users.noreply.github.com>
2025-11-17 23:02:27 -0500

    UserReview
    Class UserReview.java was properly defined with 4 sets of methods for UserReview elements & data (im sleepy so pardon lack of elaborat)
    note: subtmitReview() is yet to be properly defined.

--- src/UserReview.java
+++ src/UserReview.java
@@ -1,4 +1,53 @@
+import java.util.Scanner;
 
-public class UserReview {
+public class UserReview extends ReviewAbstract{
+	
+	private AudienceReview review;
+	
+	public void submitReview(
+		// Collect score, comment, and date, and create an AudienceReview object
+		// Update numberOfReviews 
+		String gameName,
+		String gameGenre,
+		String reviewerName,
+		String reviewerID,
+		int score,
+		String comment,
+		String date
+		) { 
+		review = new AudienceReview(
+				gameName,
+				gameGenre,
+				reviewerName,
+				reviewerID,
+				score,
+				comment,
+				date
+				);
+		numberOfReviews++;
+		
+	}
+	
+	@Override
+	public void displayReview() {
+		// Display review details (toString() for console/debugg?)
+		if (review == null) {
+			System.out.println("No review available.");
+		} else {
+			System.out.println(review.toString());
+		}
+	}
+	
+	public String getReviewText() {
+		return (review == null)
+				? "No review available."
+						: review.toString();
+	}
+
+	@Override
+	public void submitReview() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
