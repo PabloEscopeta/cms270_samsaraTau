2ddee692f7dca73a6a61e348c943ddbc22e89542
Stephan-101 <stephanazambrano@gmail.com>
2025-11-18 08:51:12 -0500

    Update UserReview.java
    I added the UserReview class information for the class

--- src/UserReview.java
+++ src/UserReview.java
@@ -1,4 +1,31 @@
 
-public class UserReview {
+public class UserReview extends ReviewAbstract {
 
+    private String reviewerName;
+    private String reviewerID;
+    private int score;
+    private String comment;
+    private String reviewDate;
+
+    // Constructor
+    public UserReview(String reviewerName, String reviewerID, int score, 
+                      String comment, String reviewDate) {
+    	
+    	
+        this.reviewerName = reviewerName;
+        this.reviewerID = reviewerID;
+        this.score = score;
+        this.comment = comment;
+        this.reviewDate = reviewDate;
+    }
+
+    
+    
+   // Will return the information the User will input themselves
+    public String toString() {
+        return "Reviewer: " + reviewerName + " (ID: " + reviewerID + ")\n" +
+               "Score: " + score + "/100\n" +
+               "Comment: " + comment + "\n" +
+               "Date made: " + reviewDate;
+    }
 }
