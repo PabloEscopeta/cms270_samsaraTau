842f5fa8eb1ffb7614d9d6f1da540550420e19ba
Stephan-101 <stephanazambrano@gmail.com>
2025-11-30 21:45:49 -0500

    Update UserReview.java
    Updated UserReview.Java

--- src/UserReview.java
+++ src/UserReview.java
@@ -1,48 +1,41 @@
-import java.util.Scanner;
-
 public class UserReview extends ReviewAbstract {
 	
-	    private String reviewerName;
-	    private String reviewerID;
-	    private int score;
-	    private String comment;
-	    private String reviewDate;
-	
-	    // Constructor
-	    public UserReview(String reviewerName, String reviewerID, int score, 
-	                      String comment, String reviewDate) {
-	    	
-	    	
-	        this.reviewerName = reviewerName;
-	        this.reviewerID = reviewerID;
-	        this.score = score;
-	        this.comment = comment;
-	        this.reviewDate = reviewDate;
-	    }
-	
-	    
-	    
-	   // Will return the information the User will input themselves
-	    public String toString() {
-	        return "Reviewer: " + reviewerName + " (ID: " + reviewerID + ")\n" +
-	               "Score: " + score + "/100\n" +
-	               "Comment: " + comment + "\n" +
-	               "Date made: " + reviewDate;
-	    }
 
+	 // The variables / data that the user will input
+    private String reviewerName;
+    private String reviewerID;
+    private int score;
+    private String comment;
+    private String reviewDate;
 
+    
+    // Constructor to help create UserReview object
+    public UserReview(String reviewerName, String reviewerID, int score, 
+                      String comment, String reviewDate) {
+    	
+   
+    	
+        this.reviewerName = reviewerName;
+        this.reviewerID = reviewerID;
+        this.score = score;
+        
+        // If the comment is left empty, will print out a "No Comment" response
+        this.comment = (comment != null && !comment.trim().isEmpty()) ? comment : "No Comment";
+        this.reviewDate = reviewDate;
+    }
 
-	@Override
-	public void submitReview() {
-		// TODO Auto-generated method stub
-		
-	}
+    
+    
+    // get method getScore
+    public int getScore() { return score; }
 
-
-
-	@Override
-	public void displayReview() {
-		// TODO Auto-generated method stub
-		
-	}
+ 
+    public String toString() {
+    	
+        return "Reviewer: " + reviewerName + " (ID: " + reviewerID + ")\n" +
+               "Score: " + score + "/100\n" + // (Score/100)
+               "Comment: " + comment + "\n" +
+               "Date: " + reviewDate + "\n";
+    }
 }
+
